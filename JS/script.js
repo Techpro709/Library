b0725946d15a932501d2e99920803f371b2f2e1d
TechPro709 <61668117+Techpro709@users.noreply.github.com>
2021-02-21 20:47:43 -0800

    editing js
    added a way to display array to webpage

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,13 @@
 console.log("Script.js is running");
 
 var books = [{id:1234,name:"test1"},{id:4321,name:"test2"},{id:1000,name:"test3"}];
+var bookdisplay = document.querySelector("#bookoptions");
+const books2 = books.map(item => {    
+    
+    console.log( item.id + " "+ item.name)
+    console.log()
+    bookdisplay.innerHTML += item.id + " " + item.name + "<br> <br>";
 
-const books2 = books.map(item => {    console.log( item.id + " "+ item.name)});
+ });
 //console.log(books2);
+
